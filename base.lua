293b2a2f31e8a003250535ed9d94d225e6420d68
nelodignaturut <108213804+nelodignaturut@users.noreply.github.com>
2023-08-05 22:48:51 +0700

    Update base.lua

--- base.lua
+++ base.lua
@@ -393,7 +393,7 @@ end)
 Section:NewSlider("Walkspeed Slider", "Same thing but slider", 25, 0, function(slowaf)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = slowaf
 end)
-Section:NewKeybind("22 WalkSpeed Keybind", "Quickchange", Unbound, function()
+Section:NewKeybind("22 WalkSpeed Keybind", "Quickchange", Enum.KeyCode.Insert, function()
 	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 22
 end)
 Section:NewLabel("WoA used to have a hard limit of 25, sometimes 22")
