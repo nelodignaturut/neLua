8ae36ebad6244506e384cc60f63b0b40d625c77d
nelodignaturut <108213804+nelodignaturut@users.noreply.github.com>
2023-08-13 22:01:39 +0700

    Update base.lua

--- base.lua
+++ base.lua
@@ -394,6 +394,40 @@ Mouse.Button1Down:connect(
         Plr.Character:MoveTo(Mouse.Hit.p)
     end)
 end)
+Section:NewButton("ESP", "Forced on function, resets on death", function()
+	local esp_settings = { 
+	    textsize = 8,
+	    colour = 255,255,255
+	}
+	 
+	local gui = Instance.new("BillboardGui")
+	local esp = Instance.new("TextLabel",gui)
+	 
+	 
+	 
+	gui.Name = "Cracked esp";
+	gui.ResetOnSpawn = false
+	gui.AlwaysOnTop = true;
+	gui.LightInfluence = 0;
+	gui.Size = UDim2.new(1.75, 0, 1.75, 0);
+	esp.BackgroundColor3 = Color3.fromRGB(255, 255, 255);
+	esp.Text = ""
+	esp.Size = UDim2.new(0.0001, 0.00001, 0.0001, 0.00001);
+	esp.BorderSizePixel = 4;
+	esp.BorderColor3 = Color3.new(esp_settings.colour)
+	esp.BorderSizePixel = 0
+	esp.Font = "GothamSemibold"
+	esp.TextSize = esp_settings.textsize
+	esp.TextColor3 = Color3.fromRGB(esp_settings.colour) -- text colour
+	 
+	game:GetService("RunService").RenderStepped:Connect(function()
+	    for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+	        if v ~= game:GetService("Players").LocalPlayer and v.Character.Head:FindFirstChild("Cracked esp")==nil  then
+	            esp.Text = "{"..v.Name.."}"
+	            gui:Clone().Parent = v.Character.Head
+	    end
+	end
+end)
 Section:NewButton("anti afk", "experimental", function()
 	local VirtualUser=game:service'VirtualUser'
 	game:service('Players').LocalPlayer.Idled:connect(function()
